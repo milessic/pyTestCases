2a8e14f343eeb2c3362d1a3581ac42def0d4676c
Miłosz Jura <milosz.m.jura@gmail.com>
2024-08-19 20:11:06 +0200

    Added custom ComboBox element

--- src/Elements.py
+++ src/Elements.py
@@ -6,6 +6,8 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt, QTimer, QPoint
 from PyQt5.QtGui import QFont, QColor
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 def myText(parent: QWidget, stylesheet, width, height, wrap=QTextEdit.WidgetWidth, **kwargs) -> QTextEdit:
     text_edit = QTextEdit(parent)
@@ -29,3 +31,31 @@ def myEntry(parent: QWidget, stylesheet, width, **kwargs) -> QLineEdit:
     line_edit.setFixedWidth(width * 10)  # approximate width conversion
     return line_edit
 
+class MyComboBox(QtWidgets.QComboBox):
+    # https://code.qt.io/cgit/qt/qtbase.git/tree/src/widgets/widgets/qcombobox.cpp?h=5.15.2#n3173
+    popupAboutToBeShown = QtCore.pyqtSignal()
+
+    def showPopup(self):
+        self.popupAboutToBeShown.emit()
+        super(MyComboBox, self).showPopup()
+
+    def paintEvent(self, event):
+        
+        painter = QtWidgets.QStylePainter(self)
+        painter.setPen(self.palette().color(QtGui.QPalette.Text))
+
+        # draw the combobox frame, focusrect and selected etc.
+        opt = QtWidgets.QStyleOptionComboBox()
+        self.initStyleOption(opt)
+        painter.drawComplexControl(QtWidgets.QStyle.CC_ComboBox, opt)
+
+        if self.currentIndex() < 0:
+            opt.palette.setBrush(
+                QtGui.QPalette.ButtonText,
+                opt.palette.brush(QtGui.QPalette.ButtonText).color().lighter(),
+            )
+            if self.placeholderText():
+                opt.currentText = self.placeholderText()
+
+        # draw the icon and text
+        painter.drawControl(QtWidgets.QStyle.CE_ComboBoxLabel, opt)
