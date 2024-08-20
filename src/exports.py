5bf59c46cd9f990c04dd57596412d96af77d6dbd
Miłosz Jura <milosz.m.jura@gmail.com>
2024-08-20 11:19:28 +0200

    fixed headers for jira formatting

--- src/exports.py
+++ src/exports.py
@@ -3,11 +3,11 @@ class Formatting:
             "jira":{
                 "bullet": "* ",
                 "ordered": "# ",
-                "h1": ".h1 ",
-                "h2": ".h2 ",
-                "h3": ".h3 ",
-                "h4": ".h4 ",
-                "h5": ".h5 ",
+                "h1": "h1. ",
+                "h2": "h2. ",
+                "h3": "h3. ",
+                "h4": "h4. ",
+                "h5": "h5. ",
                 "line_divider": "----"
                 }
             }
